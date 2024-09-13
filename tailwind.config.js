e7966296e3a94205c04749321d246469c68e4bf0
doo1b <tngml928@gmail.com>
2024-09-13 10:47:23 +0900

    style: tailwind 커스텀 폰트, 색상 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        campblue: "#1D3B41",
+        campbrown: "#6B3D1A"
+      },
+      fontFamily: {
+        preten100: "Pretendard-Thin",
+        preten200: "Pretendard-ExtraLight",
+        preten300: "Pretendard-Light",
+        preten400: "Pretendard-Regular",
+        preten500: "Pretendard-Medium",
+        preten600: "Pretendard-SemiBold",
+        preten700: "Pretendard-Bold",
+        preten800: "Pretendard-ExtraBold",
+        preten900: "Pretendard-Black"
+      }
+    }
   },
   plugins: []
 };
