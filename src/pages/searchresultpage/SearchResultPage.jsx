a0afe184e804aea2e6341aec9b65dadf4400de32
Jo <johj703@nate.com>
2024-09-13 20:37:18 +0900

    fix: 쓰지 않는 div태그 제거

--- src/pages/searchresultpage/SearchResultPage.jsx
+++ src/pages/searchresultpage/SearchResultPage.jsx
@@ -1,56 +1,48 @@
 const SearchResultPage = () => {
   return (
     <div className="container box-border gap-5 m-10 mx-auto gap-y-4">
-      <div className="">
-        <div className="flex gap-5">
-          <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
-            <p>캠핑장 사진</p>
-          </div>
-          <div className="p-3 border-2 rounded-xl w-96">
-            <p>캠핑장 이름</p>
-            <p>한 줄 소개</p>
-            <p>캠핑장 주소</p>
-            <p>전화번호</p>
-          </div>
+      <div className="flex gap-5">
+        <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
+          <p>캠핑장 사진</p>
+        </div>
+        <div className="p-3 border-2 rounded-xl w-96">
+          <p>캠핑장 이름</p>
+          <p>한 줄 소개</p>
+          <p>캠핑장 주소</p>
+          <p>전화번호</p>
         </div>
       </div>
-      <div className="">
-        <div className="flex gap-5">
-          <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
-            <p>캠핑장 사진</p>
-          </div>
-          <div className="p-3 border-2 rounded-xl w-96">
-            <p>캠핑장 이름</p>
-            <p>한 줄 소개</p>
-            <p>캠핑장 주소</p>
-            <p>전화번호</p>
-          </div>
+      <div className="flex gap-5">
+        <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
+          <p>캠핑장 사진</p>
+        </div>
+        <div className="p-3 border-2 rounded-xl w-96">
+          <p>캠핑장 이름</p>
+          <p>한 줄 소개</p>
+          <p>캠핑장 주소</p>
+          <p>전화번호</p>
         </div>
       </div>
-      <div className="">
-        <div className="flex gap-5">
-          <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
-            <p>캠핑장 사진</p>
-          </div>
-          <div className="p-3 border-2 rounded-xl w-96">
-            <p>캠핑장 이름</p>
-            <p>한 줄 소개</p>
-            <p>캠핑장 주소</p>
-            <p>전화번호</p>
-          </div>
+      <div className="flex gap-5">
+        <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
+          <p>캠핑장 사진</p>
+        </div>
+        <div className="p-3 border-2 rounded-xl w-96">
+          <p>캠핑장 이름</p>
+          <p>한 줄 소개</p>
+          <p>캠핑장 주소</p>
+          <p>전화번호</p>
         </div>
       </div>
-      <div className="">
-        <div className="flex gap-5">
-          <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
-            <p>캠핑장 사진</p>
-          </div>
-          <div className="p-3 border-2 rounded-xl w-96">
-            <p>캠핑장 이름</p>
-            <p>한 줄 소개</p>
-            <p>캠핑장 주소</p>
-            <p>전화번호</p>
-          </div>
+      <div className="flex gap-5">
+        <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
+          <p>캠핑장 사진</p>
+        </div>
+        <div className="p-3 border-2 rounded-xl w-96">
+          <p>캠핑장 이름</p>
+          <p>한 줄 소개</p>
+          <p>캠핑장 주소</p>
+          <p>전화번호</p>
         </div>
       </div>
     </div>
