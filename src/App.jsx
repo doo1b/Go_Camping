91bd4f15d812409e674677ae3aa5bec9d0e5f1a5
doo1b <tngml928@gmail.com>
2024-09-14 18:54:52 +0900

    feat: youtube data API 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/Router";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/guidepage/Modal.jsx
+++ src/components/guidepage/Modal.jsx
@@ -1,5 +1,44 @@
+import axios from "axios";
+import useGuideStore from "../../zustand/guide";
+import { useQuery } from "@tanstack/react-query";
+const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+
 const Modal = () => {
-  return <div className="w-4/5 max-w-5xl bg-white h-4/5 rounded-2xl"></div>;
+  const { searchKeyword } = useGuideStore();
+  const params = {
+    part: "snippet",
+    q: searchKeyword,
+    maxResults: 8,
+    type: "video",
+    key: YOUTUBE_API_KEY
+  };
+
+  const getYoutubeData = async () => {
+    const response = axios.get("https://www.googleapis.com/youtube/v3/search", {
+      params
+    });
+    return (await response).data.items;
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["youtube", searchKeyword],
+    queryFn: getYoutubeData,
+    staleTime: 1000 * 60 * 60 * 12, // 할당량 최적화를 위해 12시간 동안 유지
+    enabled: !!searchKeyword
+  });
+
+  console.log(data);
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  if (isError) return <div>에러 발생...</div>;
+
+  return (
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className="w-4/5 max-w-5xl bg-white h-4/5 rounded-2xl"
+    ></div>
+  );
 };
 
 export default Modal;
