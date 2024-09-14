3b4d5a5ad5fba7d77ebcf917c15a5e5ea4f45f54
jeongol <jungcode127@gmail.com>
2024-09-14 21:14:28 +0900

    add: campblue색상 버전 추가 및 메인인덱스 페이지로 변경

---
+++ src/pages/indexpage/IndexPage(white).jsx
@@ -0,0 +1,29 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import logo from "../../img/Group 357.png";
+// import place from "../../img/Group 363.png";
+// import guide from "../../img/Group 364.png";
+
+// const IndexPage = () => {
+//   const nav = useNavigate();
+
+//   return (
+//     <body>
+//       <div className="py-20">
+//         <img className="m-auto" img src={logo} alt="로고 이미지" />
+//       </div>
+//       <div>
+//         <div className="flex justify-center gap-x-20">
+//           <button type="button" onClick={() => nav("/main")}>
+//             <img src={place} alt="메인 페이지 이동 이미지" />
+//           </button>
+//           <button type="button" onClick={() => nav("/guide")}>
+//             <img src={guide} alt="영상 가이드 페이지 이미지" />
+//           </button>
+//         </div>
+//       </div>
+//     </body>
+//   );
+// };
+
+// export default IndexPage;
--- src/pages/indexpage/IndexPage.jsx
+++ src/pages/indexpage/IndexPage.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../img/Group 357.png";
-import place from "../../img/Group 363.png";
-import guide from "../../img/Group 364.png";
+import logo from "../../img/Group 356.png";
+import place from "../../img/Group 368.png";
+import guide from "../../img/Group 369.png";
 
 const IndexPage = () => {
   const nav = useNavigate();
 
   return (
-    <body>
+    <body className="bg-campblue">
       <div className="py-20">
         <img className="m-auto" img src={logo} alt="로고 이미지" />
       </div>
       <div>
         <div className="flex justify-center gap-x-20">
-          {/* <img src={logo} alt="이미지 위치 선정" /> */}
           <button type="button" onClick={() => nav("/main")}>
             <img src={place} alt="메인 페이지 이동 이미지" />
           </button>
-          {/* <img src="" alt="이미지 위치 선정" /> */}
           <button type="button" onClick={() => nav("/guide")}>
             <img src={guide} alt="영상 가이드 페이지 이미지" />
           </button>
