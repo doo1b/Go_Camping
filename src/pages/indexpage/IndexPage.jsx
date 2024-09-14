62dd81246f53679b7ca2b7e37d390f3dad403b3e
jeongol <jungcode127@gmail.com>
2024-09-14 20:55:00 +0900

    stlye: 인덱스 페이지 css 작업

--- src/pages/indexpage/IndexPage.jsx
+++ src/pages/indexpage/IndexPage.jsx
@@ -8,34 +8,23 @@ const IndexPage = () => {
   const nav = useNavigate();
 
   return (
-    <div
-      className="pt-28 pb-12"
-      style={{
-        
-      }}
-    >
+    <body>
+      <div className="py-20">
+        <img className="m-auto" img src={logo} alt="로고 이미지" />
+      </div>
       <div>
-        <div>
-          <img img src={logo} alt="로고 이미지" />
-        </div>
-        <div>
-          <div>
-            <div>
-              {/* <img src={logo} alt="이미지 위치 선정" /> */}
-              <button type="button" className="" onClick={() => nav("/main")}>
-                <img src={place} alt="메인 페이지 이동 이미지" />
-              </button>
-            </div>
-            <div>
-              {/* <img src="" alt="이미지 위치 선정" /> */}
-              <button type="button" className="" onClick={() => nav("/guide")}>
-                <img src={guide} alt="영상 가이드 페이지 이미지" />
-              </button>
-            </div>
-          </div>
+        <div className="flex justify-center gap-x-20">
+          {/* <img src={logo} alt="이미지 위치 선정" /> */}
+          <button type="button" onClick={() => nav("/main")}>
+            <img src={place} alt="메인 페이지 이동 이미지" />
+          </button>
+          {/* <img src="" alt="이미지 위치 선정" /> */}
+          <button type="button" onClick={() => nav("/guide")}>
+            <img src={guide} alt="영상 가이드 페이지 이미지" />
+          </button>
         </div>
       </div>
-    </div>
+    </body>
   );
 };
 
