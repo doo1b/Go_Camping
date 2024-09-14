444929a8ee50a86d4a8814bc9e155187de7b55c7
Jo <johj703@nate.com>
2024-09-14 21:02:16 +0900

    add: 상단에 검색 바 추가

--- src/pages/searchresultpage/SearchResultPage.jsx
+++ src/pages/searchresultpage/SearchResultPage.jsx
@@ -1,6 +1,31 @@
 const SearchResultPage = () => {
   return (
     <div className="container box-border gap-5 m-10 mx-auto gap-y-4">
+      <div className="p-5">
+        <div className="relative w-full max-w-xs">
+          <input
+            type="text"
+            className="w-full py-2 pl-4 pr-10 border rounded-xl focus:outline-none"
+            placeholder="캠핑장을 검색해 보세요."
+          />
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+            <svg
+              className="w-5 h-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-4.35-4.35m1.35-5.65a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
       <div className="flex gap-5">
         <div className="flex items-center justify-center h-32 border-2 w-28 rounded-xl">
           <p>캠핑장 사진</p>
