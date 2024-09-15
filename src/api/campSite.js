871d9dfb7a501552e527a512b9d423ea6857cf2f
lukby2457 <kjh7442455@gmail.com>
2024-09-15 20:31:52 +0900

    feat: 고캠핑 API에서 db.json으로 데이터 삽입하는 임시 페이지 제작 완료

--- src/api/campSite.js
+++ src/api/campSite.js
@@ -2,7 +2,22 @@ import axios from "axios";
 
 const DB_JSON_URL = "http://localhost:5000/campsiteList";
 
+export const getTotalCount = async () => {
+  const response = await axios.get(`${DB_JSON_URL}?_page=1&_per_page=1`)
+  return response.data;
+}
+
+export const getCampsites = async () => {
+  const response = await axios.get(`${DB_JSON_URL}`);
+  return response.data;
+}
+
 export const getOneCampsite = async (contentId) => {
   const response = await axios.get(`${DB_JSON_URL}?contentId=${contentId.contentId}`);
+  return response.data;
+}
+
+export const deleteOneCampsite = async (id) => {
+  const response = await axios.delete(`${DB_JSON_URL}/${id}`)
   return response.data;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import App from "./App.jsx";
 import "./style/main.css";
 import "./style/reset.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
