f1da519c86a6384004eaa478da167d4ccd9ca7c7
lukby2457 <kjh7442455@gmail.com>
2024-09-16 20:37:33 +0900

    update: 위도 경도 정보를 바탕으로 kakao지도 및 마커 추가

--- src/pages/detailpage/DetailPage.jsx
+++ src/pages/detailpage/DetailPage.jsx
@@ -1,7 +1,9 @@
 import { Link, useParams } from "react-router-dom"
 import { useEffect } from "react";
 import { useState } from "react";
-import { getOneCampsite } from "../../api/campSite";
+import { getOneCampsite, getTotalCount } from "../../api/campSite";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import DetailPageLogo from "../../assets/DetailPageLogo";
 
 const AmenitiesInfo = ({ caravInnerFclty, glampInnerFclty }) => {
   return (
@@ -41,6 +43,32 @@ const FacilityInfo = ({ sbrsCl, sbrsEtc }) => {
   );
 }
 
+const CampsiteMap = ({ lat, lng }) => {
+  return (
+    <Map // 지도를 표시할 Container
+      center={{
+        // 지도의 중심좌표
+        lat: lat,
+        lng: lng,
+      }}
+      style={{
+        // 지도의 크기
+        width: "100%",
+        height: "300px",
+      }}
+      level={4} // 지도의 확대 레벨
+    >
+      <MapMarker // 마커를 생성합니다
+        position={{
+          // 마커가 표시될 위치입니다
+          lat: lat,
+          lng: lng,
+        }}
+      />
+    </Map>
+  )
+}
+
 const DetailPage = () => {
   const contentId = useParams('contentId');
   const [campsite, setCampsite] = useState(null);
@@ -48,6 +76,8 @@ const DetailPage = () => {
   useEffect(() => {
     const getCampsite = async (contentId) => {
       try {
+        const count = await getTotalCount();
+        console.log(count);
         const data = await getOneCampsite(contentId);
         setCampsite(data[0]);
       } catch (error) {
@@ -62,13 +92,13 @@ const DetailPage = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center mb-3 gap-5">
+      <div className="flex items-center justify-center gap-5 mb-3">
         <h1 className="text-3xl font-preten500">{campsite?.facltNm}</h1>
         <h4 className="flex text-[17px] font-preten400">{campsite?.induty}</h4>
       </div>
       <div className="flex flex-wrap">
         <section className="min-w-[400px] min-h-[300px]">
-          <img src={campsite?.firstImageUrl} />
+        <>{ campsite?.firstImageUrl !== "" ? <img src={campsite?.firstImageUrl} /> : <DetailPageLogo /> }</>
         </section>
         <section className="min-w-[400px] min-h-[300px]">
           <>{ campsite?.lineIntro && <h4 className="text-[17px] font-preten400 mb-2">{campsite.lineIntro}</h4> }</>
@@ -76,15 +106,14 @@ const DetailPage = () => {
           <FacilityInfo sbrsCl={campsite?.sbrsCl} sbrsEtc={campsite?.sbrsEtc} />
           <h4 className="text-[17px] font-preten400 mb-2">운영 기간</h4>
           <p className="text-[17px] font-preten300 mb-2">{campsite?.operPdCl} / {campsite?.operDeCl}</p>
-          <div className="flex gap-3 items-center">
+          <div className="flex items-center gap-3">
             <h4 className="text-[17px] font-preten400 mb-2">애완동물 출입</h4><p className="text-[17px] font-preten300 mb-2">{campsite?.animalCmgCl}</p>
           </div>
           <>{ campsite?.tel && <p className="text-[17px] font-preten300 mb-2">전화 문의: {campsite?.tel}</p> }</>
           <>{ campsite?.homepage && <Link to={campsite?.homepage}>홈페이지 바로가기</Link> }</>
         </section>
         <section className="min-w-[400px] min-h-[300px]">
-          <p>위도: {campsite?.mapX}</p>
-          <p>경도: {campsite?.mapY}</p>
+          <CampsiteMap lat={campsite?.mapY} lng={campsite?.mapX} />
           <p className="text-[17px] font-preten300 mb-2">캠핑장 주소: {campsite?.addr1}</p>
         </section>
       </div>
