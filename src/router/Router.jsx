4ec86218ecf887e643bfdd797ec88a1d939fa3ca
Jo <johj703@nate.com>
2024-09-19 17:12:02 +0900

    fix: 필요없는 부분 삭제

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexPage from "../pages/indexpage/IndexPage";
 import MainPage from "../pages/mainpage/MainPage";
