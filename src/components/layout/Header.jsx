56cf16aa6d262030797877fd57b0d30d2992f5c8
jeongol <jungcode127@gmail.com>
2024-09-19 22:17:56 +0900

    refactor: 인덱스 페이지/헤더 컴포넌트 css완성 및 컴포넌트 리펙토링

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,26 +3,18 @@ import place from "../../../src/assets/header/header_place.png";
 import guide from "../../../src/assets/header/header_guide.png";
 import WeatherDisplay from "../../components/mainpage/WeatherDisplay";
 import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
+import SearchDisplay from "../mainpage/SearchDisplay";
+
 
 const Header = () => {
-  const [searchForm, setSearchForm] = useState("");
   const location = useLocation();
-
-  const submitSearchForm = (e) => {
-    e.preventDefault();
-  };
-  //  날씨 컴포넌트 헤더 수직 너비에 맞추기
-  // 준현님 검색결과 페이지에 검색바 연동
 
   const onWeatherDisplay = location.pathname.startsWith("/detail");
 
   return (
     <header className="bg-campblue p-4 flex items-center justify-between sticky top-0 z-50 h-[90px]">
       <div className="flex-shrink-0">
-        {/* <Link to="/" target="_blank" rel="noopener noreferrer"> */}
         <img src={logo} alt="로고 이미지 홈으로 이동" />
-        {/* </Link> */}
       </div>
       {onWeatherDisplay && (
         <div className="text-sm">
@@ -32,32 +24,19 @@ const Header = () => {
       <div className="flex items-center flex-shrink-0 space-x-6">
         <Link
           to="/main"
-          target="_blank"
-          rel="noopener noreferrer"
           className="transition-transform duration-300 transform hover:scale-110"
         >
-          <img src={place} alt="지도로 가기" />
+          <img src={place} alt="지도로 이동" />
         </Link>
         <Link
           to="/guide"
-          target="_blank"
-          rel="noopener noreferrer"
           className="transition-transform duration-300 transform hover:scale-110"
         >
-          <img src={guide} alt="지도로 가기" />
+          <img src={guide} alt="가이드 영상으로 아동" />
         </Link>
-        <form
-          onSubmit={submitSearchForm}
-          className="w-[255px] h-[45px] flex justify-between items-center px-4 z-10 rounded-full shadow-mainBoxShadow bg-white transition-transform duration-300 transform hover:scale-105"
-        >
-          <input
-            type="text"
-            value={searchForm}
-            onChange={(e) => setSearchForm(e.target.value)}
-            placeholder="캠핑장 검색하기!"
-            className="pl-1 outline-none"
-          />
-        </form>
+        <div className="w-[265px] h-[45px]">
+          <SearchDisplay />
+        </div>
       </div>
     </header>
   );
--- src/pages/indexpage/IndexPage.jsx
+++ src/pages/indexpage/IndexPage.jsx
@@ -6,11 +6,8 @@ import guide from "../../../src/assets/indexpages/index_guide.png";
 
 const IndexPage = () => {
   const nav = useNavigate();
-  {
-    /* <div className="bg-campblue min-h-screen flex flex-col justify-center items-center"> */
-  }
   return (
-    <div className="flex flex-col justify-center min-h-screen bg-campblue">
+    <div className="bg-campblue min-h-screen flex flex-col justify-center items-center">
       <div className="py-10">
         <img className="m-auto" src={logo} alt="로고 이미지" />
       </div>
@@ -19,14 +16,14 @@ const IndexPage = () => {
           <button
             type="button"
             onClick={() => nav("/main")}
-            className="transition transform hover:bg-opacity-110 active:scale-90 transition-transform duration-300 transform hover:scale-110"
+            className="hover:bg-opacity-110 active:scale-90 transition-transform duration-300 transform hover:scale-110"
           >
             <img key="true" src={place} alt="메인 페이지 이동 이미지" />
           </button>
           <button
             type="button"
             onClick={() => nav("/guide")}
-            className="transition transform hover:bg-opacity-110 active:scale-90 transition-transform duration-300 transform hover:scale-110"
+            className="hover:bg-opacity-110 active:scale-90 transition-transform duration-300 transform hover:scale-110"
           >
             <img key="true" src={guide} alt="영상 가이드 페이지 이미지" />
           </button>
