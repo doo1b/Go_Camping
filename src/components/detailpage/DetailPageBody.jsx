3a2d0402448ed8b0cd3f3df08e70be8b32a60652
doo1b <tngml928@gmail.com>
2024-09-21 18:38:13 +0900

    style: 검색 결과 페이지 스타일 레이아웃 변경, 클릭시 상세페이지 이동

--- src/components/detailpage/DetailPageBody.jsx
+++ src/components/detailpage/DetailPageBody.jsx
@@ -1,21 +1,27 @@
-import DetailPageLogo from '../../assets/DetailPageLogo';
-import DetailInfo from './DetailInfo';
-import DetailMapSection from './DetailMapSection';
+import DetailPageLogo from "../../assets/DetailPageLogo";
+import DetailInfo from "./DetailInfo";
+import DetailMapSection from "./DetailMapSection";
 
 const DetailPageBody = ({ campsite }) => {
   return (
     <>
       <div className="flex flex-wrap justify-center gap-10">
         <section className="detailSection w-[600px]">
-          <>{ campsite?.firstImageUrl !== "" ? <img src={campsite?.firstImageUrl} className='rounded-2xl' /> : <DetailPageLogo /> }</>
+          <>
+            {campsite?.firstImageUrl !== "" ? (
+              <img src={campsite?.firstImageUrl} className="rounded-2xl" />
+            ) : (
+              <DetailPageLogo />
+            )}
+          </>
         </section>
         <DetailInfo campsite={campsite} />
       </div>
-      <div className='w-[600px] mt-10 mx-auto'>
+      <div className="w-[600px] mt-10 mx-auto">
         <DetailMapSection campsite={campsite} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DetailPageBody
+export default DetailPageBody;
--- src/components/searchresultpage/SearchResults.jsx
+++ src/components/searchresultpage/SearchResults.jsx
@@ -1,22 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { useLocationStore } from "../../store/locationStore";
+
 const SearchResults = ({ filteredCamps }) => {
+  const { setCampsite } = useLocationStore();
+  const navgate = useNavigate();
+
   return (
-    <ul>
+    <ul className="grid grid-cols-[repeat(2,minmax(500px,1fr))] gap-y-10 gap-x-20 border-t-[1px] pt-10 mt-2">
       {filteredCamps.map((camp) => (
         <li
           key={camp.contentId}
-          className="flex p-4 mb-6 bg-white border border-gray-300 rounded-lg shadow-md"
+          className="flex flex-row h-48 border-b-[1px] pb-10"
+          onClick={() => {
+            setCampsite(camp), navgate(`/detail/${camp.contentId}`);
+          }}
         >
           <img
-            src={camp.firstImageUrl}
+            src={
+              camp.firstImageUrl
+                ? camp.firstImageUrl
+                : "https://images.unsplash.com/photo-1496947850313-7743325fa58c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
             alt={camp.name}
-            className="object-cover w-32 h-32 mr-4 rounded-lg"
-          />
-          <div>
-            <h3>{camp.facltNm}</h3>
-            {/* <p>{camp.name}</p> */}
-            <p>{camp.lineIntro}</p>
-            <p>{camp.addr}</p>
-            <p>{camp.tel}</p>
+            className="object-cover aspect-[3/2] mr-4 rounded-xl"
+          />{" "}
+          <div className="flex flex-col justify-between py-1">
+            <p className="text-lg font-preten600">{camp.facltNm}</p>
+            <p className="text-sm">{camp.lineIntro}</p>
+            <div className="text-sm">
+              <p>{camp.addr1}</p>
+              <p>{camp.tel ? camp.tel : "번호 정보 없음"}</p>
+            </div>
           </div>
         </li>
       ))}
